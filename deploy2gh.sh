4b5d3958192a835cb08d50237e6141256238bb43
Ariel Noyman <noyman@mit.edu>
2019-03-13 20:49:40 -0400

    moving MapBox GLJS to dedicate repo

--- deploy2gh.sh
+++ deploy2gh.sh
@@ -2,7 +2,7 @@
 # clear dist folder 
 sudo rm -rf dist
 # build the dist for public url 
-sudo parcel build CSjs_MAPBOX-GL-JS/index.html --public-url https://cityscope.media.mit.edu/CS_CityScopeJS_Simulation/
+sudo parcel build CSjs_WALKABILITY/index.html --public-url https://cityscope.media.mit.edu/CS_CityScopeJS_Simulation/
 # make sure to add dist to commit if .gitignored 
 git add dist -f
 #commit the GH pages changes 
